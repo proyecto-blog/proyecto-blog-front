db84ce48849f4e7ee902de39a7c0594d0a014091
Yadira Henao <yadira.henao@udea.edu.co>
2021-12-06 09:46:15 -0500

    Inicio de la creación del proyecto de un blog

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hola, este será un blog
+        </p>
+      </header>      
+    </div>
+  );
+};
+
+export default Index;
+
+
